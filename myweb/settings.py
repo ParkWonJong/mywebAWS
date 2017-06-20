ee2f0487c263a32603067844cad5123cf2010815
ParkWonJong <dnjswhd403@naver.com>
2017-06-21 02:25:46 +0900

    Update settings.py

--- myweb/settings.py
+++ myweb/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'Baseball',
         'USER': 'root',
         'PASSWORD':'',
-        'HOST':'localhost',
+        'HOST':'127.0.0.1',
         'PORT':'3306',
         'OPTIONS':{
             'init_command': "SET sql_mode = 'STRICT_TRANS_TABLES'",
