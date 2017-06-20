5fb23ea0003939f645eeace63d1bc0529bcec0e5
ParkWonJong <dnjswhd403@naver.com>
2017-06-21 04:13:04 +0900

    Update models.py

--- home/models.py
+++ home/models.py
@@ -20,25 +20,19 @@ class Post(models.Model):
         return self.title
 
 
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=254)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
+class Authuser(models.Model):
+    id = models.CharField(primary_key=True, max_length=50)
+    email = models.CharField(max_length=50, blank=True, null=True)
+    password = models.CharField(max_length=30, blank=True, null=True)
+    score = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'auth_user'
+        db_table = 'authuser'
 
 
 class Beacon(models.Model):
-    seq = models.AutoField(db_column='Seq', primary_key=True)  # Field name made lowercase.
+    seq = models.IntegerField(db_column='Seq', primary_key=True)  # Field name made lowercase.
     uuid = models.CharField(db_column='UUID', max_length=50)  # Field name made lowercase.
     value = models.IntegerField(db_column='Value')  # Field name made lowercase.
 
